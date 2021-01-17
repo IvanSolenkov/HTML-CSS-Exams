6f305f0454ada0bd53e21b799520a96d0292624e
Ivan Solenkov <ivan_solenkov1999@abv.bg>
2021-01-17 19:35:01 +0200

    New items added

---
+++ Exam 3/01. Login/script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('login-button')
+
+button.addEventListener('click', buttonEvent)
+
+function buttonEvent() {
+    window.location.href = "/Visual%20Studio%20Code/11.Exam 2/03. Certificates-Landing-Page/index.html";
+}
